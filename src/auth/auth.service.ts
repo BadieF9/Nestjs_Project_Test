aa0d09350aa1cc0bc09c80f10a3e5503ad3ebaad
Badie <badie.farzandi@gmail.com>
2024-12-03 21:13:15 +0330

    Auth service structure improved

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,21 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Farmer } from 'src/entities/farmer.entity';
-import { Repository } from 'typeorm';
+import { FarmersService } from 'src/farmers/farmers.service';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectRepository(Farmer)
-    private farmersRepository: Repository<Farmer>,
+    private farmersService: FarmersService,
     private jwtService: JwtService,
   ) {}
 
   async signIn(email: string, pass: string): Promise<{ access_token: string }> {
-    const farmer = await this.farmersRepository.findOneBy({ email });
-    
+    const farmer = await this.farmersService.getFarmerByEmail(email);
+
     if (!farmer || farmer?.password !== pass) {
       throw new UnauthorizedException();
     }
-    
+
     const payload = { sub: farmer.id, email: farmer.email };
     return {
       access_token: await this.jwtService.signAsync(payload),
